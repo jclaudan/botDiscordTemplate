3148df602b19b9b59ddcd4090668ce86ca1afedf
jclaudan <36130708+jclaudan@users.noreply.github.com>
2021-11-04 00:55:13 +0100

    refactor memberManager

--- base.js
+++ base.js
@@ -11,14 +11,13 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.js');
 const { selectMenuRow, buttonMessage, embedMessage } = require('./utils.js');
+const { mute, unmute, warn, kick, ban } = require('./memberManager.js');
 
 const warns = require("./warns.json");
 const fs = require('fs');
 const ms = require('ms');
 // Require the necessary discord.js classes
 // const { token } = require('./config.json');
-
-const defaultTimeMute = '30s';
 
 // Create a new client instance
 const client = new Client({ 
@@ -90,141 +89,21 @@ rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error)
 
+    const defaultTimeMute = '30s';
+
     client.on('interactionCreate', async interaction => {
         if (!interaction.isCommand()) return;
         const { commandName, memberPermissions } = interaction;
         if (commandName === 'mute') {
-            const reason = interaction.options.getString('reason');
-            let time = interaction.options.getString('time');
-
-            if (!memberPermissions.has(Permissions.FLAGS.MUTE_MEMBERS))
-                return interaction.reply("You do not have permission to do that.");
-            const user = interaction.options.getUser('target');
-
-            if (!user)
-                return interaction.reply("Please specify someone you want to mute. **!mute <user> [time] [reason]**");
-            const target = interaction.member.guild.members.cache.get(user.id);
-
-            if (!time)
-                time = defaultTimeMute;
-                const memberVoiceState = target.guild.voiceStates.cache.get(target.id);
-                await memberVoiceState.setMute(true, reason)
-
-            const embed = new MessageEmbed()
-                .setColor("#00aaaa")
-                .setDescription(`${user} has been muted by @${interaction.user.username} for ${ms(ms(time))}.\nReason: **${reason != "" ? reason : "-"}**`);
-
-            await interaction.reply({ embeds: [embed] });
-
-            setTimeout(async () => {
-                await memberVoiceState.setMute(false, reason)
-                const unmute = new MessageEmbed()
-                    .setColor("#00aaaa")
-                    .setDescription(`${user} has been unmuted.`);
-                await interaction.followUp({ embeds: [unmute] });
-            }, ms(time));
-
+            await mute(interaction)
         } else if (commandName === 'unmute') {
-            const reason = interaction.options.getString('reason');
-
-            if (!memberPermissions.has(Permissions.FLAGS.MUTE_MEMBERS))
-                return interaction.reply("You do not have permission to do that.");
-            const user = interaction.options.getUser('target');
-            if (!user)
-                return interaction.reply("Please specify someone you want to unmute. **!unmute <user> [reason]**");
-            const target = interaction.member.guild.members.cache.get(user.id);
-
-            const memberVoiceState = target.guild.voiceStates.cache.get(target.id);
-                await memberVoiceState.setMute(true, reason)
-
-            const unmute = new MessageEmbed()
-                .setColor("#00aaaa")
-                .setDescription(`${user} has been unmuted by @${interaction.user.username}.\nReason: **${reason != "" ? reason : "-"}**`);
-                await interaction.reply({ embeds: [unmute] });
-        
+            await unmute(interaction)
         } else if (commandName === 'warn') {
-            const reason = interaction.options.getString('reason');
-
-            if (!memberPermissions.has(Permissions.FLAGS.MUTE_MEMBERS))
-                return interaction.reply("You do not have permission to do that.");
-            const user = interaction.options.getUser('target');
-            if (!user)
-                return interaction.reply("Please specify someone you want to warn. **!warn <user> [reason]**");
-            const target = interaction.member.guild.members.cache.get(user.id);
-
-            if (!warns[user.id]) {
-                warns[user.id] = {
-                    warnCount: 1
-                };
-            } else {
-                warns[user.id].warnCount += 1;
-            }
-
-            if (warns[user.id].warnCount >= 3) {
-                const mute = new MessageEmbed()
-                    .setColor("#00aaaa")
-                    .setDescription(`${user} has been muted. (**3**/**3**)\nReason: **${reason != "" ? reason : "-"}**`);
-                await interaction.reply({ embeds: [mute] });
-
-                const memberVoiceState = target.guild.voiceStates.cache.get(user.id)
-                await memberVoiceState.setMute(true, reason)
-                warns[user.id].warnCount = 0;
-
-                setTimeout(async () => {
-                    await memberVoiceState.setMute(false, reason)
-                    const unmute = new MessageEmbed()
-                        .setColor("#00aaaa")
-                        .setDescription(`${user} has been unmuted.`);
-                    await interaction.followUp({ embeds: [unmute] });
-                }, ms(defaultTimeMute));
-
-            } else {
-                const warn = new MessageEmbed()
-                    .setColor("#00aaaa")
-                    .setDescription(`${user} has been warned by @${interaction.user.username}. (**${warns[user.id].warnCount}**/**3**) \nReason: **${reason != "" ? reason : "-"}**`);
-                await interaction.reply({ embeds: [warn] });
-            }
-
-            fs.writeFile("./warns.json", JSON.stringify(warns), err => {
-                if (err)
-                    console.log(err);
-            });
-
+           await warn(interaction)
         } else if (commandName === 'kick') {
-            const reason = interaction.options.getString('reason');
-
-            if (!memberPermissions.has("KICK_MEMBERS"))
-                return interaction.reply("You do not have permission to do that.");
-            const user = interaction.options.getUser('target');
-            if (!user)
-                return interaction.reply("Please specify someone you want to kick. **!kick <user> [reason]**");
-            if (user.id === interaction.user.id)
-                return interaction.reply("You cannot kick yourself.");
-
-            await interaction.member.guild.members.cache.get(user.id).kick(reason);
-
-            const kickmessage = new MessageEmbed()
-                .setColor("#00aaaa")
-                .setDescription(`${user} has been kicked. Reason: **${reason != "" ? reason : "-"}**`);
-            await interaction.reply({ embeds: [kickmessage] });
-
+            await kick(interaction)
         } else if (commandName === 'ban') {
-            const reason = interaction.options.getString('reason');
-
-            if (!memberPermissions.has("BAN_MEMBERS"))
-                return interaction.reply("You do not have permission to do that.");
-            const user = interaction.options.getUser('target');
-            if (!user)
-                return interaction.reply("Please specify someone you want to ban. **!ban <user> [reason]**");
-            if (user.id === interaction.user.id)
-                return interaction.reply("You cannot ban yourself.");
-
-            await interaction.member.guild.members.cache.get(user.id).ban({ reason: reason });
-
-            const banmessage = new MessageEmbed()
-                .setColor("#00aaaa")
-                .setDescription(`${user} has been banned. Reason: **${reason != "" ? reason : "-"}**`);
-            await interaction.reply({ embeds: [banmessage] });
+            await ban(interaction)
         }
         
     });
---
+++ memberManager.js
@@ -0,0 +1,149 @@
+const { MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu } = require('discord.js');
+const defaultTimeMute = '30s';
+
+const mute = async (interaction) => {
+    const reason = interaction.options.getString('reason');
+    let time = interaction.options.getString('time');
+
+    if (!memberPermissions.has(Permissions.FLAGS.MUTE_MEMBERS))
+        return interaction.reply("You do not have permission to do that.");
+    const user = interaction.options.getUser('target');
+
+    if (!user)
+        return interaction.reply("Please specify someone you want to mute. **!mute <user> [time] [reason]**");
+    const target = interaction.member.guild.members.cache.get(user.id);
+
+    if (!time)
+        time = defaultTimeMute;
+        const memberVoiceState = target.guild.voiceStates.cache.get(target.id);
+        await memberVoiceState.setMute(true, reason)
+
+    const embed = new MessageEmbed()
+        .setColor("#00aaaa")
+        .setDescription(`${user} has been muted by @${interaction.user.username} for ${ms(ms(time))}.\nReason: **${reason != "" ? reason : "-"}**`);
+
+    await interaction.reply({ embeds: [embed] });
+
+    setTimeout(async () => {
+        await memberVoiceState.setMute(false, reason)
+        const unmute = new MessageEmbed()
+            .setColor("#00aaaa")
+            .setDescription(`${user} has been unmuted.`);
+        await interaction.followUp({ embeds: [unmute] });
+    }, ms(time));
+}
+
+const unmute = async (interaction) => {
+    const reason = interaction.options.getString('reason');
+
+    if (!memberPermissions.has(Permissions.FLAGS.MUTE_MEMBERS))
+        return interaction.reply("You do not have permission to do that.");
+    const user = interaction.options.getUser('target');
+    if (!user)
+        return interaction.reply("Please specify someone you want to unmute. **!unmute <user> [reason]**");
+    const target = interaction.member.guild.members.cache.get(user.id);
+
+    const memberVoiceState = target.guild.voiceStates.cache.get(target.id);
+    await memberVoiceState.setMute(true, reason)
+
+    const unmute = new MessageEmbed()
+        .setColor("#00aaaa")
+        .setDescription(`${user} has been unmuted by @${interaction.user.username}.\nReason: **${reason != "" ? reason : "-"}**`);
+    await interaction.reply({ embeds: [unmute] });
+
+}
+
+const warn = async (interaction) => {
+    const reason = interaction.options.getString('reason');
+
+    if (!memberPermissions.has(Permissions.FLAGS.MUTE_MEMBERS))
+        return interaction.reply("You do not have permission to do that.");
+    const user = interaction.options.getUser('target');
+    if (!user)
+        return interaction.reply("Please specify someone you want to warn. **!warn <user> [reason]**");
+    const target = interaction.member.guild.members.cache.get(user.id);
+
+    if (!warns[user.id]) {
+        warns[user.id] = {
+            warnCount: 1
+        };
+    } else {
+        warns[user.id].warnCount += 1;
+    }
+
+    if (warns[user.id].warnCount >= 3) {
+        const mute = new MessageEmbed()
+            .setColor("#00aaaa")
+            .setDescription(`${user} has been muted. (**3**/**3**)\nReason: **${reason != "" ? reason : "-"}**`);
+        await interaction.reply({ embeds: [mute] });
+
+        const memberVoiceState = target.guild.voiceStates.cache.get(user.id)
+        await memberVoiceState.setMute(true, reason)
+        warns[user.id].warnCount = 0;
+
+        setTimeout(async () => {
+            await memberVoiceState.setMute(false, reason)
+            const unmute = new MessageEmbed()
+                .setColor("#00aaaa")
+                .setDescription(`${user} has been unmuted.`);
+            await interaction.followUp({ embeds: [unmute] });
+        }, ms(defaultTimeMute));
+
+    } else {
+        const warn = new MessageEmbed()
+            .setColor("#00aaaa")
+            .setDescription(`${user} has been warned by @${interaction.user.username}. (**${warns[user.id].warnCount}**/**3**) \nReason: **${reason != "" ? reason : "-"}**`);
+        await interaction.reply({ embeds: [warn] });
+    }
+
+    fs.writeFile("./warns.json", JSON.stringify(warns), err => {
+        if (err)
+            console.log(err);
+    });
+}
+
+const kick = async (interaction) => {
+    const reason = interaction.options.getString('reason');
+
+    if (!memberPermissions.has("KICK_MEMBERS"))
+        return interaction.reply("You do not have permission to do that.");
+    const user = interaction.options.getUser('target');
+    if (!user)
+        return interaction.reply("Please specify someone you want to kick. **!kick <user> [reason]**");
+    if (user.id === interaction.user.id)
+        return interaction.reply("You cannot kick yourself.");
+
+    await interaction.member.guild.members.cache.get(user.id).kick(reason);
+
+    const kickmessage = new MessageEmbed()
+        .setColor("#00aaaa")
+        .setDescription(`${user} has been kicked. Reason: **${reason != "" ? reason : "-"}**`);
+    await interaction.reply({ embeds: [kickmessage] });
+}
+
+const ban = async (interaction) => {
+    const reason = interaction.options.getString('reason');
+
+    if (!memberPermissions.has("BAN_MEMBERS"))
+        return interaction.reply("You do not have permission to do that.");
+    const user = interaction.options.getUser('target');
+    if (!user)
+        return interaction.reply("Please specify someone you want to ban. **!ban <user> [reason]**");
+    if (user.id === interaction.user.id)
+        return interaction.reply("You cannot ban yourself.");
+
+    await interaction.member.guild.members.cache.get(user.id).ban({ reason: reason });
+
+    const banmessage = new MessageEmbed()
+        .setColor("#00aaaa")
+        .setDescription(`${user} has been banned. Reason: **${reason != "" ? reason : "-"}**`);
+    await interaction.reply({ embeds: [banmessage] });
+}
+
+module.exports = {
+    mute,
+    unmute,
+    warn,
+    kick,
+    ban,
+}
